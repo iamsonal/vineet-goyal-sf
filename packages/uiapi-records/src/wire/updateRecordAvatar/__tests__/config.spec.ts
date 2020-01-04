a845f5e6fde80d770a2802961b1e55e06959b62f
Vince Chen <vince.chen@salesforce.com>
2020-01-04 13:46:28 +0800

    chore: setup sfci (#4)

--- packages/uiapi-records/src/wire/updateRecordAvatar/__tests__/config.spec.ts
+++ packages/uiapi-records/src/wire/updateRecordAvatar/__tests__/config.spec.ts
@@ -3,7 +3,7 @@ import { factory as updateRecordAvatar } from '../index';
 describe('validation', () => {
     it('throws error with bad config', async () => {
         try {
-            await expect(updateRecordAvatar({} as any)(undefined as any));
+            await updateRecordAvatar({} as any)(undefined as any);
         } catch (e) {
             expect(e.message).toBe('updateRecordAvatar invalid configuration');
         }

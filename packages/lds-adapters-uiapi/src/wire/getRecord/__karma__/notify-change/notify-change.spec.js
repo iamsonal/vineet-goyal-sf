9b76481cfa677e0811914f0429c552ac6e814648
Vince Chen <vince.chen@salesforce.com>
2020-02-28 16:05:45 -0800

    test(uiapi): more getRecordNotifyChange tests (#77)

--- packages/lds-adapters-uiapi/src/wire/getRecord/__karma__/notify-change/notify-change.spec.js
+++ packages/lds-adapters-uiapi/src/wire/getRecord/__karma__/notify-change/notify-change.spec.js
@@ -43,4 +43,132 @@ describe('notify change', () => {
         expect(element.pushCount()).toBe(2);
         expect(element.getWiredData()).toEqualSnapshotWithoutEtags(refreshMockData);
     });
+
+    it('does not return optionalFields that were there previously', async () => {
+        const initialMock = getMock('record-TestD__c-fields-TestD__c.TestC__r.Id-populated');
+        const initialConfig = {
+            recordId: initialMock.id,
+            optionalFields: ['TestD__c.TestC__r.Id'],
+        };
+
+        mockGetRecordNetwork(initialConfig, initialMock);
+
+        const refreshMock = getMock('record-TestD__c-TestD__c.TestC__r-missing-updated');
+        const refreshConfig = {
+            recordId: refreshMock.id,
+            optionalFields: [
+                'TestD__c.CreatedBy.Id',
+                'TestD__c.CreatedBy.Name',
+                'TestD__c.CreatedById',
+                'TestD__c.CreatedDate',
+                'TestD__c.LastModifiedBy.Id',
+                'TestD__c.LastModifiedBy.Name',
+                'TestD__c.LastModifiedById',
+                'TestD__c.LastModifiedDate',
+                'TestD__c.Name',
+                'TestD__c.Owner.Id',
+                'TestD__c.Owner.Name',
+                'TestD__c.OwnerId',
+                'TestD__c.TestC__c',
+                'TestD__c.TestC__r.Id',
+            ],
+        };
+
+        // need two network mocks to satisfy notifyChange + buildNetworkSnapshot from mergeAndRefreshHigherVersionRecord
+        mockGetRecordNetwork(refreshConfig, [refreshMock, refreshMock]);
+
+        const element = await setupElement(initialConfig, RecordFields);
+        const representation = element.getWiredData();
+
+        await element.notifyChange([{ recordId: representation.id }]);
+
+        expect(element.pushCount()).toBe(2);
+
+        const expected = {
+            ...refreshMock,
+            fields: {},
+        };
+
+        expect(element.getWiredData()).toEqualSnapshotWithoutEtags(expected);
+    });
+
+    it('returns a populated lookup field that was previously null', async () => {
+        const initialMock = getMock('record-TestD__c-fields-TestD__c.TestC__r-null');
+        const initialConfig = {
+            recordId: initialMock.id,
+            optionalFields: ['TestD__c.TestC__r.Id'],
+        };
+
+        const refreshMock = getMock('record-TestD__c-fields-TestD__c.TestC__r.Id-populated');
+
+        mockGetRecordNetwork(initialConfig, [initialMock, refreshMock]);
+
+        const element = await setupElement(initialConfig, RecordFields);
+        const representation = element.getWiredData();
+
+        await element.notifyChange([{ recordId: representation.id }]);
+
+        expect(element.pushCount()).toBe(2);
+
+        const expected = {
+            ...refreshMock,
+            fields: {
+                TestC__r: refreshMock.fields.TestC__r,
+            },
+        };
+
+        expect(element.getWiredData()).toEqualSnapshotWithoutEtags(expected);
+    });
+
+    it('returns a null lookup field that was previously populated', async () => {
+        const initialMock = getMock('record-TestD__c-fields-TestD__c.TestC__r.Id-populated');
+        const initialConfig = {
+            recordId: initialMock.id,
+            optionalFields: ['TestD__c.TestC__r.Id'],
+        };
+
+        mockGetRecordNetwork(initialConfig, initialMock);
+
+        const refreshMock = getMock('record-TestD__c-TestD__c.TestC__r-null-refreshed');
+        const refreshconfig = {
+            recordId: refreshMock.id,
+            optionalFields: [
+                'TestD__c.CreatedBy.Id',
+                'TestD__c.CreatedBy.Name',
+                'TestD__c.CreatedById',
+                'TestD__c.CreatedDate',
+                'TestD__c.LastModifiedBy.Id',
+                'TestD__c.LastModifiedBy.Name',
+                'TestD__c.LastModifiedById',
+                'TestD__c.LastModifiedDate',
+                'TestD__c.Name',
+                'TestD__c.Owner.Id',
+                'TestD__c.Owner.Name',
+                'TestD__c.OwnerId',
+                'TestD__c.TestC__c',
+                'TestD__c.TestC__r.Id',
+            ],
+        };
+
+        mockGetRecordNetwork(refreshconfig, refreshMock);
+
+        const element = await setupElement(initialConfig, RecordFields);
+        const representation = element.getWiredData();
+
+        await element.notifyChange([{ recordId: representation.id }]);
+
+        expect(element.pushCount()).toBe(2);
+
+        const expected = {
+            ...refreshMock,
+            fields: {
+                TestC__r: {
+                    value: null,
+                    displayValue: null,
+                },
+            },
+        };
+
+        expect(element.getWiredData()).toEqualSnapshotWithoutEtags(expected);
+    });
 });
---
+++ packages/lds-adapters-uiapi/src/wire/getRecord/__tests__/notify-change.spec.ts
@@ -0,0 +1,12 @@
+import { Store, LDS } from '@ldsjs/engine';
+import { notifyChangeFactory } from '../index';
+
+describe('notify change', () => {
+    it('does not refresh when record is not in store', () => {
+        const mockNetworkAdapter = jest.fn();
+        const mockLds = new LDS(new Store(), mockNetworkAdapter);
+        const getRecordNotifyChange = notifyChangeFactory(mockLds);
+        getRecordNotifyChange([{ recordId: '123' }]);
+        expect(mockNetworkAdapter).not.toHaveBeenCalled();
+    });
+});

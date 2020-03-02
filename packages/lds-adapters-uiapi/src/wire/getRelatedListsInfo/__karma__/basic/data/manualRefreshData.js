91b78c01c40b96091f137aa27cda8a4cec096044
Stephen Carraway <57263085+scarrawaySF@users.noreply.github.com>
2020-03-02 10:37:24 -0500

    fix(W-7266742) update infos wire to incl label (#72)
    * fix(W-7266742) update infos wire to incl label
    
    * Update refresh data

--- packages/lds-adapters-uiapi/src/wire/getRelatedListsInfo/__karma__/basic/data/manualRefreshData.js
+++ packages/lds-adapters-uiapi/src/wire/getRelatedListsInfo/__karma__/basic/data/manualRefreshData.js
@@ -10,7 +10,7 @@ const helpers = require(helpersPath);
 // W-6594688 PLEASE NOTE: For the time being, this refresh data script does not work against the standard mock-data-app
 // When this comment is removed the file should be renamed back to refreshData.js
 
-const PARENT_ENTITY_API_NAME = 'CObjParent__c';
+const PARENT_ENTITY_API_NAME = 'CwcCustom00__c';
 const MASTER_RECORD_TYPE_ID = '012000000000000AAA';
 
 await helpers.requestGetAndSave(

2cac11cb0f50adfd99ae6d61f4152aea2a7937fd
Wes Peter <wpeter@salesforce.com>
2020-06-18 12:26:56 -0700

    chore(mobile): remove usage of makeOffline (#267)

--- packages/lds-runtime-mobile/src/main.ts
+++ packages/lds-runtime-mobile/src/main.ts
@@ -1,9 +1,9 @@
-import { LDS, Store, Environment, makeOffline } from '@ldsjs/engine';
+import { LDS, Store, Environment } from '@ldsjs/engine';
 
 import { NimbusNetworkAdapter } from './NimbusNetworkAdapter';
 
 const store = new Store();
-const env = makeOffline(new Environment(store, NimbusNetworkAdapter));
+const env = new Environment(store, NimbusNetworkAdapter);
 const lds = new LDS(env);
 
 export { lds };

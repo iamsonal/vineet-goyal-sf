e54dffad608da297180ac5abd803365078674a3c
Wes Peter <wpeter@salesforce.com>
2020-07-01 11:44:54 -0700

    chore: add refreshData scripts for offline test mockData (#309)
    * start offline test for lists and refactor to use resolveUnfulfilled
    
    * cleanup little bit
    
    * remove list stuff for now
    
    * put comment at top

---
+++ packages/lds-adapters-uiapi/src/wire/getLayout/__tests__/mockData/refreshData.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env sfdx repl:script
+
+const path = require('path');
+
+const helpersPath = process.argv[process.argv.length - 1];
+const rootDir = path.dirname(process.argv[3]);
+
+const helpers = require(helpersPath);
+
+await helpers.requestGetAndSave(
+    '/ui-api/layout/Opportunity',
+    path.join(rootDir, 'layout-Opportunity-Full.json')
+);
---
+++ packages/lds-adapters-uiapi/src/wire/getPicklistValues/__tests__/mockData/refreshData.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env sfdx repl:script
+
+const path = require('path');
+
+const helpersPath = process.argv[process.argv.length - 1];
+const rootDir = path.dirname(process.argv[3]);
+
+const helpers = require(helpersPath);
+
+const MASTER_RECORD_TYPE_ID = '012000000000000AAA';
+
+const entries = [
+    {
+        objectApiName: 'Account',
+        recordTypeId: MASTER_RECORD_TYPE_ID,
+        fieldApiName: 'Type',
+        filename: 'picklist-Account-MasterRecordTypeId-fieldApiName-Type',
+    },
+];
+
+// /services/data/v50.0/ui-api/object-info/Account/picklist-values/012000000000000AAA/Type
+entries.forEach(async ({ fieldApiName, filename, objectApiName, recordTypeId }) => {
+    await helpers.requestGetAndSave(
+        `/ui-api/object-info/${objectApiName}/picklist-values/${recordTypeId}/${fieldApiName}`,
+        path.join(rootDir, `${filename}.json`)
+    );
+});

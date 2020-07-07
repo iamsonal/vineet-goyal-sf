0599c181fc1451cd5314abc178c0427b8587cc04
Justin Horst <justin.horst@salesforce.com>
2020-07-07 09:59:59 -0700

    test(mobile): Add test for deleteRecord to ensure durable store entry is deleted (#314)
    * add offline test for deleteRecord
    
    * bump package versions
    
    * fix test
    
    * rename test

---
+++ packages/lds-adapters-uiapi/src/wire/deleteRecord/__test__/offline.spec.ts
@@ -0,0 +1,46 @@
+import response from './data/record-User-fields-User.Id,User.Name.json';
+import { buildOfflineLds, populateDurableStore } from '@salesforce/lds-jest';
+import {
+    buildMockNetworkAdapter,
+    MockPayload,
+    buildSuccessMockPayload,
+} from '@ldsjs/adapter-test-library';
+import { factory as deleteRecordFactory } from '../index';
+import { factory as getRecordFactory } from '../../getRecord/index';
+import { keyBuilder as recordKeyBuilder } from '../../../generated/types/RecordRepresentation';
+
+const RECORD_ID = '005xx000001XL1tAAG';
+
+const getArgs: MockPayload['networkArgs'] = {
+    method: 'get',
+    basePath: `/ui-api/records/${RECORD_ID}`,
+};
+
+const deleteArgs: MockPayload['networkArgs'] = {
+    method: 'delete',
+    basePath: `/ui-api/records/${RECORD_ID}`,
+};
+
+const getPayload: MockPayload = buildSuccessMockPayload(getArgs, response);
+const deletePayload: MockPayload = buildSuccessMockPayload(deleteArgs, {});
+
+const getConfig = {
+    recordId: RECORD_ID,
+    fields: ['User.Id', 'User.Name'],
+};
+
+function flushPromises() {
+    return new Promise(resolve => setImmediate(resolve));
+}
+
+describe('deleteRecord offline tests', () => {
+    it('evicts the entry in durable store', async () => {
+        const durableStore = await populateDurableStore(getRecordFactory, getConfig, getPayload);
+        const { lds } = buildOfflineLds(durableStore, buildMockNetworkAdapter([deletePayload]));
+        const adapter = deleteRecordFactory(lds);
+        await (adapter(RECORD_ID) as Promise<void>);
+        await flushPromises();
+        const key = recordKeyBuilder({ recordId: RECORD_ID });
+        expect(durableStore.entries[key]).toBeUndefined();
+    });
+});

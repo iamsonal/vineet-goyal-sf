55183cfc8de8e03358dfaedc4cb582fa49fe1451
Josh-Goodman <josh.goodman@salesforce.com>
2020-10-16 10:30:30 -0700

    feat: Offline getRecords tests (TDD) (#487)
    * Initial commit for offline tests
    
    * Add work item to the xit tests
    
    * Merge latest.
    
    * Fix getRecordsAdapterFactory import

---
+++ packages/lds-adapters-uiapi/src/wire/getRecords/__tests__/offline.spec.ts
@@ -0,0 +1,150 @@
+import { LDS, Store, Environment, NetworkAdapter } from '@ldsjs/engine';
+import {
+    buildMockNetworkAdapter,
+    buildSuccessMockPayload,
+    MockPayload,
+    MockDurableStore,
+    getMockNetworkAdapterCallCount,
+} from '@ldsjs/adapter-test-library';
+
+import { responseRecordRepresentationRetrievers } from '../../../generated/records/retrievers';
+
+import { makeDurable, makeOffline } from '@ldsjs/environments';
+import {
+    keyBuilder,
+    RecordRepresentationNormalized,
+} from '../../../generated/types/RecordRepresentation';
+
+import multipleRecordsWithIdName from './mockData/records-multiple-Accounts-fields-Account.Id,Account.Name.json';
+import multipleRecordsWithIdNameNickColor from './mockData/records-multiple-Accounts-fields-Account.Id,Account.Name,Account.NickName,Account.Color.json';
+import { ObjectKeys } from '../../../generated/types/type-utils';
+import { testDurableHitDoesNotHitNetwork } from '@salesforce/lds-jest';
+import { getRecordsAdapterFactory } from '../../../generated/adapters/getRecords';
+
+const recordId_Account1 = '001xx000003Gn4WAAS';
+const recordId_Account2 = '001xx000003Gn4WAAT';
+
+const recordKey1 = keyBuilder({ recordId: recordId_Account1 });
+
+const recordFields_Account = ['Account.Id', 'Account.Name'];
+const batchRecordRequest_Account: MockPayload['networkArgs'] = {
+    method: 'get',
+    basePath: `/ui-api/records/batch/${recordId_Account1},${recordId_Account2}`,
+    queryParams: { fields: recordFields_Account },
+};
+const recordPayload_Account: MockPayload = buildSuccessMockPayload(
+    batchRecordRequest_Account,
+    multipleRecordsWithIdName
+);
+
+function buildLds(durableStore: MockDurableStore, n?: NetworkAdapter) {
+    const store = new Store();
+    const network = n ?? buildMockNetworkAdapter([recordPayload_Account]);
+
+    const env = makeDurable(
+        makeOffline(new Environment(store, network)),
+        durableStore,
+        responseRecordRepresentationRetrievers
+    );
+
+    const lds = new LDS(env);
+    return {
+        lds,
+        durableStore,
+        network,
+        store,
+        env,
+    };
+}
+
+async function populateDurableStore() {
+    const { durableStore, lds, network } = buildLds(
+        new MockDurableStore(),
+        buildMockNetworkAdapter([recordPayload_Account])
+    );
+
+    const adapter = getRecordsAdapterFactory(lds);
+    const snapshotOrPromise = adapter({
+        records: [
+            {
+                recordIds: [recordId_Account1, recordId_Account2],
+                fields: recordFields_Account,
+            },
+        ],
+    }) as Promise<any>;
+    expect(snapshotOrPromise).toBeInstanceOf(Promise);
+
+    const result = await snapshotOrPromise;
+    expect(result.state).toEqual('Fulfilled');
+    const callCount = getMockNetworkAdapterCallCount(network);
+    expect(callCount).toBe(1);
+    return durableStore;
+}
+
+// This is required because broadcast does not await the write to
+// durable store so we need to make sure the microtask queue is emptied
+// before checking the durable store for expected result
+function flushPromises() {
+    return new Promise(resolve => setImmediate(resolve));
+}
+
+// TODO: W-8207118
+describe('getRecords with fields offline', () => {
+    xit('does not hit the network when all fields are in durable store', async () => {
+        const config = {
+            records: [
+                {
+                    recordIds: [recordId_Account1, recordId_Account2],
+                    fields: recordFields_Account,
+                },
+            ],
+        };
+        await testDurableHitDoesNotHitNetwork(
+            getRecordsAdapterFactory,
+            config,
+            recordPayload_Account
+        );
+    });
+
+    // TODO: W-8207118
+    xit('merges with fields that are in durable store', async () => {
+        const durableStore = await populateDurableStore();
+
+        const request: MockPayload['networkArgs'] = {
+            method: 'get',
+            basePath: `/ui-api/records/batch/${recordId_Account1},${recordId_Account2}`,
+            queryParams: { fields: ['Account.Color', 'Account.Id', 'Account.NickName'] },
+        };
+        const payload: MockPayload = buildSuccessMockPayload(
+            request,
+            multipleRecordsWithIdNameNickColor
+        );
+
+        const network = buildMockNetworkAdapter([payload]);
+        const { lds, store } = buildLds(durableStore, network);
+        // test the record doesn't exist in memory
+        expect(store.records[recordKey1]).toBeUndefined();
+
+        const adapter = getRecordsAdapterFactory(lds);
+        const result = await (adapter({
+            records: [
+                {
+                    recordIds: [recordId_Account1],
+                    fields: ['Account.Id', 'Account.NickName', 'Account.Color'],
+                },
+            ],
+        }) as Promise<any>);
+        await flushPromises();
+
+        expect(result.state).toEqual('Fulfilled');
+
+        const mergedFields = ['Id', 'Name', 'NickName', 'Color'];
+
+        const record = store.records[recordKey1] as RecordRepresentationNormalized;
+        expect(ObjectKeys(record.fields)).toEqual(mergedFields);
+
+        const durableRecord = durableStore.entries[recordKey1]
+            .data as RecordRepresentationNormalized;
+        expect(ObjectKeys(durableRecord.fields)).toEqual(mergedFields);
+    });
+});

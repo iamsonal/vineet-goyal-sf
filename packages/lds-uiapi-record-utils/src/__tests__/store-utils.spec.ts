fbd2639f833de98b7a02f18827fb57d56f885c51
Justin Horst <justin.horst@salesforce.com>
2020-10-19 16:57:32 -0700

    record key utils (#504)

--- packages/lds-uiapi-record-utils/src/__tests__/store-utils.spec.ts
+++ packages/lds-uiapi-record-utils/src/__tests__/store-utils.spec.ts
@@ -1,19 +1,22 @@
 import {
+    extractRecordIdFromStoreKey,
     isStoreKeyRecordField,
     isStoreKeyRecordId,
     isStoreKeyRecordOrRecordField,
 } from '../store-utils';
-const RECORD_ID = 'UiApi::RecordRepresentation:001T1000001nFt5IAE';
-const RECORD_FIELD_ID = 'UiApi::RecordRepresentation:001T1000001nFt5IAE__fields__Name';
+
+const RECORD_ID = '001T1000001nFt5IAE';
+const RECORD_KEY = 'UiApi::RecordRepresentation:001T1000001nFt5IAE';
+const RECORD_FIELD_KEY = 'UiApi::RecordRepresentation:001T1000001nFt5IAE__fields__Name';
 const RANDOM_STORE_ID = 'UiApi::Random:001T1000001nFt5IAE';
 describe('store-utils', () => {
     describe('isStoreKeyRecordId', () => {
         it('returns true for a record id', () => {
-            expect(isStoreKeyRecordId(RECORD_ID)).toEqual(true);
+            expect(isStoreKeyRecordId(RECORD_KEY)).toEqual(true);
         });
 
         it('returns false for a record field key', () => {
-            expect(isStoreKeyRecordId(RECORD_FIELD_ID)).toBe(false);
+            expect(isStoreKeyRecordId(RECORD_FIELD_KEY)).toBe(false);
         });
 
         it('returns false for a random key', () => {
@@ -23,11 +26,11 @@ describe('store-utils', () => {
 
     describe('isStoreKeyRecordField', () => {
         it('returns true for a record field key', () => {
-            expect(isStoreKeyRecordField(RECORD_FIELD_ID)).toBe(true);
+            expect(isStoreKeyRecordField(RECORD_FIELD_KEY)).toBe(true);
         });
 
         it('returns false for a record id', () => {
-            expect(isStoreKeyRecordField(RECORD_ID)).toBe(false);
+            expect(isStoreKeyRecordField(RECORD_KEY)).toBe(false);
         });
 
         it('returns false for a random key', () => {
@@ -37,11 +40,11 @@ describe('store-utils', () => {
 
     describe('isStoreKeyRecordOrRecordField', () => {
         it('returns true for a record key', () => {
-            expect(isStoreKeyRecordOrRecordField(RECORD_ID)).toBe(true);
+            expect(isStoreKeyRecordOrRecordField(RECORD_KEY)).toBe(true);
         });
 
         it('returns true for a record field key', () => {
-            expect(isStoreKeyRecordOrRecordField(RECORD_FIELD_ID)).toBe(true);
+            expect(isStoreKeyRecordOrRecordField(RECORD_FIELD_KEY)).toBe(true);
         });
 
         it('returns false for a random key', () => {
@@ -50,6 +53,20 @@ describe('store-utils', () => {
 
         it('undefined returns false', () => {
             expect(isStoreKeyRecordOrRecordField(undefined)).toBe(false);
+        });
+    });
+
+    describe('extractRecordIdFromStoreKey', () => {
+        it('returns record id from record key', () => {
+            expect(extractRecordIdFromStoreKey(RECORD_KEY)).toBe(RECORD_ID);
+        });
+
+        it('returns record id from record field key', () => {
+            expect(extractRecordIdFromStoreKey(RECORD_FIELD_KEY)).toBe(RECORD_ID);
+        });
+
+        it('returns undefined for random key', () => {
+            expect(extractRecordIdFromStoreKey(RANDOM_STORE_ID)).toBe(undefined);
         });
     });
 });
--- packages/lds-uiapi-record-utils/src/store-utils.ts
+++ packages/lds-uiapi-record-utils/src/store-utils.ts
@@ -3,6 +3,8 @@ const RECORD_ID_REGEXP = new RegExp(`^${RECORD_ID_PREFIX}([a-zA-Z0-9])+$`);
 const RECORD_FIELD_REGEXP = new RegExp(
     `^${RECORD_ID_PREFIX}([a-zA-Z0-9]+)+__fields__([a-zA-Z0-9]+)+$`
 );
+const RECORD_FIELDS_KEY_JUNCTION = '__fields__';
+const RECORD_OR_RECORD_FIELD_REGEXP = new RegExp(`^${RECORD_ID_PREFIX}([a-zA-Z0-9]+)+.*$`);
 
 export function isStoreKeyRecordId(key: string) {
     return RECORD_ID_REGEXP.test(key);
@@ -17,4 +19,16 @@ export function isStoreKeyRecordOrRecordField(key: string) {
         return false;
     }
     return key.startsWith(RECORD_ID_PREFIX);
+}
+
+export function extractRecordIdFromStoreKey(key: string) {
+    const matches = key.match(RECORD_OR_RECORD_FIELD_REGEXP);
+    if (!matches || matches.length !== 2) {
+        return undefined;
+    }
+    return matches[1];
+}
+
+export function buildRecordFieldStoreKey(recordKey: string, fieldName: string) {
+    return `${recordKey}${RECORD_FIELDS_KEY_JUNCTION}${fieldName}`;
 }

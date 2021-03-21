725b3d748e5fa3bc2f842243a415451efa8831ec
Mike Burr <39976424+mburr-salesforce@users.noreply.github.com>
2021-03-20 21:24:32 -0400

    feat(w-8998973): add missing exports to lwc-adapters-uiapi (#847)
    * add ts-jest dependency
    
    * export bound adapters from @salesforce/lds-adapters-uiapi
    
    * clean up LWC stanzas in package.jsons
    
    * undo ts-jest dependency
    
    * revert to old factory-only content for @salesforce/lds-adapters-uiapi
    
    * add remaining exports to lwc-adapters-uiapi

--- packages/lwc-adapters-uiapi/src/main.ts
+++ packages/lwc-adapters-uiapi/src/main.ts
@@ -1,13 +1,23 @@
 import { Luvio } from '@luvio/engine';
+import { bindWireRefresh } from '@luvio/lwc-luvio';
 import { withDefaultLuvio } from '@salesforce/lds-default-luvio';
 import { GetRecordNotifyChange as notifyChangeFactory } from '@salesforce/lds-adapters-uiapi';
 
-export { MRU } from '@salesforce/lds-adapters-uiapi';
+export {
+    createRecordInputFilteredByEditedFields,
+    generateRecordInputForCreate,
+    generateRecordInputForUpdate,
+    getFieldDisplayValue,
+    getRecordInput,
+    getFieldValue,
+    MRU,
+} from '@salesforce/lds-adapters-uiapi';
 
-export let getRecordNotifyChange: any;
+export let getRecordNotifyChange: any, refresh: any;
 
 withDefaultLuvio((luvio: Luvio) => {
     getRecordNotifyChange = notifyChangeFactory(luvio);
+    refresh = bindWireRefresh(luvio);
 });
 
 export * from './generated/artifacts/bound-adapters';

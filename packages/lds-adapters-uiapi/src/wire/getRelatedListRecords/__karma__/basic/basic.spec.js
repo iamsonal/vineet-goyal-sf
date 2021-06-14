5a5ce0dd466fc25415e88dc42ebe9cb32673644b
Cheng Li <cheng.li@salesforce.com>
2021-06-14 11:05:44 -0700

    test: fix and enable couple RL records tests (#1113)

--- packages/lds-adapters-uiapi/src/wire/getRelatedListRecords/__karma__/basic/basic.spec.js
+++ packages/lds-adapters-uiapi/src/wire/getRelatedListRecords/__karma__/basic/basic.spec.js
@@ -166,8 +166,7 @@ describe('basic', () => {
         expect(element.pushCount()).toBe(2);
     });
 
-    // Test disabled until W-7096496 is fixed
-    xit('delete record refreshes related list records', async () => {
+    it('delete record refreshes related list records', async () => {
         const mockData = getMock('related-list-records-Custom');
         const refreshedMockData = getMock('related-list-records-Custom');
 
@@ -178,6 +177,7 @@ describe('basic', () => {
         };
 
         const recordId = refreshedMockData.records.splice(0, 1)[0].id;
+        refreshedMockData.count = refreshedMockData.count - 1;
         mockGetRelatedListRecordsNetwork(resourceConfig, [mockData, refreshedMockData]);
 
         const props = {
@@ -198,11 +198,11 @@ describe('basic', () => {
         expect(element.pushCount()).toBe(2);
     });
 
-    // Test disabled until W-7096496 is fixed
-    xit('refresh list with deleted records', async () => {
+    it('refresh list with deleted records', async () => {
         const mockData = getMock('related-list-records-Custom');
         const refreshedMockData = getMock('related-list-records-Custom');
         refreshedMockData.records.splice(0, 1); // remove the first record
+        refreshedMockData.count = refreshedMockData.count - 1;
 
         const resourceConfig = {
             parentRecordId: mockData.listReference.inContextOfRecordId,

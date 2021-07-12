409e9bca7b0e9b865b1df03700d385446a142c0d
rax-it <rakshit.verma@salesforce.com>
2021-07-12 14:06:58 -0400

    fix(release): update rollup (#1231)

--- packages/lds-adapters-cms-type/rollup.config.js
+++ packages/lds-adapters-cms-type/rollup.config.js
@@ -1,6 +1,8 @@
 import { rollup } from '../../scripts/rollup/rollup.config.adapters';
 import path from 'path';
 
+import * as packageJson from './package.json';
+
 const sfdcEntry = path.join(__dirname, 'src', 'generated', 'artifacts', 'sfdc.ts');
 const entry = path.join(__dirname, 'src', 'generated', 'artifacts', 'main.ts');
 
@@ -10,4 +12,5 @@ export default rollup({
     entry,
     fileName: 'cms-type',
     bundleName: 'cmsType',
+    packageVersion: packageJson.version,
 });

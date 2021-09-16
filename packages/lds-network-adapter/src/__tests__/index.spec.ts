a61215bdcc5666d1b51b7e1a8603de8a7b00aa85
David Turissini <dturissini@salesforce.com>
2021-09-16 13:56:16 -0700

    feat(network): adding unit tests (#1373)
    * adding tests
    * pr feedback
    * Testing signing
    * testing signing
    * testing signing

--- packages/lds-network-adapter/src/__tests__/index.spec.ts
+++ packages/lds-network-adapter/src/__tests__/index.spec.ts
@@ -1,5 +1,6 @@
 import { ResourceRequest } from '@luvio/engine';
 import platformNetworkAdapter from '../main';
+import { UI_API_BASE_URI } from '../uiapi-base';
 import { buildResourceRequest } from './test-utils';
 
 function testControllerInput(request: Partial<ResourceRequest>, expectedResponseBody?) {
@@ -19,21 +20,25 @@ function testControllerInput(request: Partial<ResourceRequest>, expectedResponse
 function testRejectFetchResponse(request: Partial<ResourceRequest>) {
     test('rejects an instance of FetchError the controller throws', async () => {
         const fn = jest.fn().mockRejectedValueOnce({
-            data: {
-                statusCode: 400,
-                message: 'Invalid request',
-            },
-        });
-
-        const { rejects } = await expect(platformNetworkAdapter(fn)(buildResourceRequest(request)));
-
-        rejects.toMatchObject({
             status: 400,
             body: {
                 statusCode: 400,
                 message: 'Invalid request',
             },
         });
+
+        try {
+            await platformNetworkAdapter(fn)(buildResourceRequest(request));
+            throw new Error('Test failure: No error thrown');
+        } catch (e) {
+            expect(e).toEqual({
+                status: 400,
+                body: {
+                    statusCode: 400,
+                    message: 'Invalid request',
+                },
+            });
+        }
     });
 }
 
@@ -82,6 +87,304 @@ describe('routes', () => {
             {
                 id: 'hello',
                 title: 'world',
+            }
+        );
+    });
+
+    describe('post /records', () => {
+        testControllerInput({
+            method: 'post',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records`,
+            body: {
+                apiName: 'Test__c',
+                fields: [],
+            },
+        });
+
+        describe('with query params', () => {
+            testControllerInput({
+                method: 'post',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records`,
+                body: {
+                    apiName: 'Test__c',
+                    fields: [],
+                },
+                queryParams: {
+                    triggerUserEmail: true,
+                },
+            });
+        });
+
+        testRejectFetchResponse({
+            method: 'post',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records`,
+        });
+
+        testResolveResponse(
+            {
+                method: 'post',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records`,
+                body: {
+                    apiName: 'Test__c',
+                    fields: [],
+                },
+            },
+            {
+                id: '1234',
+                apiName: 'Test__c',
+                fields: {},
+            }
+        );
+    });
+
+    describe('get /records/{recordId}', () => {
+        testRejectFetchResponse({
+            method: 'get',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records/1234`,
+            urlParams: {
+                recordId: '1234',
+            },
+        });
+
+        testResolveResponse(
+            {
+                method: 'get',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/1234`,
+                urlParams: {
+                    recordId: '1234',
+                },
+                queryParams: {
+                    fields: ['Id'],
+                },
+            },
+            {
+                id: '1234',
+                fields: {
+                    Id: {
+                        value: '1234',
+                    },
+                },
+            }
+        );
+
+        describe('with fields', () => {
+            testControllerInput({
+                method: 'get',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/1234`,
+                urlParams: {
+                    recordId: '1234',
+                },
+                queryParams: {
+                    fields: ['Id'],
+                    optionalFields: [],
+                },
+            });
+        });
+
+        describe('with optional fields', () => {
+            testControllerInput({
+                method: 'get',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/1234`,
+                urlParams: {
+                    recordId: '1234',
+                },
+                queryParams: {
+                    fields: [],
+                    optionalFields: ['Name'],
+                },
+            });
+        });
+
+        describe('with optional and required fields', () => {
+            testControllerInput({
+                method: 'get',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/1234`,
+                urlParams: {
+                    recordId: '1234',
+                },
+                queryParams: {
+                    fields: ['Id'],
+                    optionalFields: ['Name'],
+                },
+            });
+        });
+
+        describe('with layout', () => {
+            testControllerInput({
+                method: 'get',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/1234`,
+                urlParams: {
+                    recordId: '1234',
+                },
+                queryParams: {
+                    layoutTypes: ['Full'],
+                    modes: ['View'],
+                    optionalFields: ['Name'],
+                },
+            });
+        });
+    });
+
+    describe('patch /records/{recordId}', () => {
+        testControllerInput({
+            method: 'patch',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records/1234`,
+            urlParams: {
+                recordId: '1234',
+            },
+            body: {
+                apiName: 'Test__c',
+                fields: [],
+            },
+            headers: {
+                'If-Modified-Since': '1234',
+            },
+        });
+
+        testControllerInput({
+            method: 'patch',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records/1234`,
+            urlParams: {
+                recordId: '1234',
+            },
+            body: {
+                apiName: 'Test__c',
+                fields: [],
+            },
+            headers: {
+                'If-Unmodified-Since': '1234',
+            },
+        });
+
+        describe('with query params', () => {
+            testControllerInput({
+                method: 'patch',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/1234`,
+                urlParams: {
+                    recordId: '1234',
+                },
+                queryParams: {
+                    useDefaultRule: true,
+                },
+                body: {
+                    apiName: 'Test__c',
+                    fields: [],
+                },
+                headers: {
+                    'If-Modified-Since': '1234',
+                },
+            });
+        });
+
+        testRejectFetchResponse({
+            method: 'patch',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records/1234`,
+        });
+
+        testResolveResponse(
+            {
+                method: 'patch',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/1234`,
+                urlParams: {
+                    recordId: '1234',
+                },
+                body: {
+                    apiName: 'Test__c',
+                    fields: [],
+                },
+            },
+            {
+                id: '1234',
+                apiName: 'Test__c',
+                fields: {},
+            }
+        );
+    });
+
+    describe('delete /records/{recordId}', () => {
+        testControllerInput({
+            method: 'delete',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records/1234`,
+            urlParams: {
+                recordId: '1234',
+            },
+            body: {},
+        });
+
+        testRejectFetchResponse({
+            method: 'delete',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records/1234`,
+        });
+
+        testResolveResponse(
+            {
+                method: 'delete',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/1234`,
+                urlParams: {
+                    recordId: '1234',
+                },
+                body: {},
+            },
+            null
+        );
+    });
+
+    describe('get /records/batch/{recordIds}', () => {
+        testControllerInput({
+            method: 'get',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records/batch/1234,5678`,
+            urlParams: {
+                recordIds: ['1234', '5678'],
+            },
+            queryParams: {
+                fields: ['Id'],
+                optionalFields: ['Name'],
+            },
+        });
+
+        testRejectFetchResponse({
+            method: 'get',
+            baseUri: UI_API_BASE_URI,
+            basePath: `/records/batch/1234,5678`,
+            urlParams: {
+                recordIds: ['1234', '5678'],
+            },
+        });
+
+        testResolveResponse(
+            {
+                method: 'get',
+                baseUri: UI_API_BASE_URI,
+                basePath: `/records/batch/1234,5678`,
+                urlParams: {
+                    recordIds: ['1234', '5678'],
+                },
+            },
+            {
+                hasErrors: false,
+                fields: [
+                    { statusCode: 200, result: {} },
+                    { statusCode: 200, result: {} },
+                ],
             }
         );
     });
---
+++ packages/lds-network-adapter/src/uiapi-base.ts
@@ -0,0 +1,1 @@
+export const UI_API_BASE_URI = '/services/data/v54.0/ui-api';

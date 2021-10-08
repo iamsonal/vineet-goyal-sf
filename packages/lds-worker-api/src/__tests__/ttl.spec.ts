9b8f730caf79dbccdc0a05b3d96ece0136321bf3
David Turissini <dturissini@salesforce.com>
2021-10-08 11:07:10 -0700

    fix(worker): adding guards for configurable ttl methods (#1468)
    * adding guards to ttl methods
    * pr feedback

--- packages/lds-worker-api/src/__tests__/ttl.spec.ts
+++ packages/lds-worker-api/src/__tests__/ttl.spec.ts
@@ -2,30 +2,146 @@ import { luvio } from '@salesforce/lds-runtime-mobile';
 import { API_NAMESPACE, RECORD_REPRESENTATION_NAME } from '@salesforce/lds-uiapi-record-utils';
 import { setUiApiRecordTTL, setMetadataTTL } from '../ttl';
 
-describe('Configurable TTL', () => {
-    describe('setUiApiRecordTTL', () => {
-        it('calls luvio.storeSetTTLOverride with the right params', () => {
-            const TTL_VALUE = 1000;
-            const storeSetTTLOverrideSpy = jest.spyOn(luvio, 'storeSetTTLOverride');
+async function mockImportChain() {
+    let mockProvideLuvio = (_mockLuvio) => {
+        throw new Error('Test error: withDefaultLuvio not called yet');
+    };
+    const storeSetTTLOverrideSpy = jest.fn();
+    const storeSetDefaultTTLOverrideSpy = jest.fn();
+    const mockLuvio = {
+        storeSetDefaultTTLOverride: storeSetDefaultTTLOverrideSpy,
+        storeSetTTLOverride: storeSetTTLOverrideSpy,
+    };
+    jest.mock('@salesforce/lds-runtime-mobile', () => {
+        const actual = jest.requireActual('@salesforce/lds-runtime-mobile');
+        return {
+            ...actual,
+            withDefaultLuvio: (cb) => {
+                mockProvideLuvio = cb;
+            },
+        };
+    });
+    const { setUiApiRecordTTL, setMetadataTTL } = await import('../ttl');
+    return {
+        storeSetTTLOverrideSpy,
+        storeSetDefaultTTLOverrideSpy,
+        setUiApiRecordTTL,
+        setMetadataTTL,
+        mockLuvio,
+        mockProvideLuvio,
+    };
+}
 
-            setUiApiRecordTTL(TTL_VALUE);
-            expect(storeSetTTLOverrideSpy).toHaveBeenCalledTimes(1);
-            expect(storeSetTTLOverrideSpy).toHaveBeenCalledWith(
-                API_NAMESPACE,
-                RECORD_REPRESENTATION_NAME,
-                TTL_VALUE
-            );
+describe('Configurable TTL', () => {
+    describe('Before default luvio has been provided', () => {
+        beforeEach(() => {
+            jest.resetModules();
+        });
+
+        afterEach(() => {
+            jest.resetModules();
+        });
+        describe('setUiApiRecordTTL', () => {
+            it('calls luvio.storeSetTTLOverride with the right params', async () => {
+                const { mockProvideLuvio, storeSetTTLOverrideSpy, setUiApiRecordTTL, mockLuvio } =
+                    await mockImportChain();
+
+                const TTL_VALUE = 1000;
+
+                setUiApiRecordTTL(TTL_VALUE);
+                expect(storeSetTTLOverrideSpy).not.toHaveBeenCalled();
+                mockProvideLuvio(mockLuvio);
+                expect(storeSetTTLOverrideSpy).toHaveBeenCalledTimes(1);
+                expect(storeSetTTLOverrideSpy).toHaveBeenCalledWith(
+                    API_NAMESPACE,
+                    RECORD_REPRESENTATION_NAME,
+                    TTL_VALUE
+                );
+            });
+
+            it('calls luvio.storeSetTTLOverride with the right params when setUiApiRecordTTL invoked multiple times before luvio available', async () => {
+                const { mockProvideLuvio, storeSetTTLOverrideSpy, setUiApiRecordTTL, mockLuvio } =
+                    await mockImportChain();
+
+                const TTL_VALUE = 1000;
+
+                setUiApiRecordTTL(344);
+                setUiApiRecordTTL(TTL_VALUE);
+                expect(storeSetTTLOverrideSpy).not.toHaveBeenCalled();
+                mockProvideLuvio(mockLuvio);
+                expect(storeSetTTLOverrideSpy).toHaveBeenCalledTimes(1);
+                expect(storeSetTTLOverrideSpy).toHaveBeenCalledWith(
+                    API_NAMESPACE,
+                    RECORD_REPRESENTATION_NAME,
+                    TTL_VALUE
+                );
+            });
+        });
+
+        describe('setMetadataTTL', () => {
+            it('calls luvio.storeSetDefaultTTLOverride with the right params', async () => {
+                const {
+                    mockProvideLuvio,
+                    storeSetDefaultTTLOverrideSpy,
+                    setMetadataTTL,
+                    mockLuvio,
+                } = await mockImportChain();
+
+                const TTL_VALUE = 1000;
+                setMetadataTTL(TTL_VALUE);
+                expect(storeSetDefaultTTLOverrideSpy).not.toHaveBeenCalled();
+                mockProvideLuvio(mockLuvio);
+                expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledTimes(1);
+                expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledWith(TTL_VALUE);
+            });
+
+            it('calls luvio.storeSetDefaultTTLOverride with the right params when setUiApiRecordTTL invoked multiple times before luvio available', async () => {
+                const {
+                    mockProvideLuvio,
+                    storeSetDefaultTTLOverrideSpy,
+                    setMetadataTTL,
+                    mockLuvio,
+                } = await mockImportChain();
+
+                const TTL_VALUE = 1000;
+
+                setMetadataTTL(344);
+                setMetadataTTL(TTL_VALUE);
+                expect(storeSetDefaultTTLOverrideSpy).not.toHaveBeenCalled();
+                mockProvideLuvio(mockLuvio);
+                expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledTimes(1);
+                expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledWith(TTL_VALUE);
+            });
         });
     });
+    describe('After default luvio has been provided', () => {
+        describe('setUiApiRecordTTL', () => {
+            it('calls luvio.storeSetTTLOverride with the right params', () => {
+                const TTL_VALUE = 1000;
+                const storeSetDefaultTTLOverrideSpy = jest.spyOn(luvio, 'storeSetTTLOverride');
 
-    describe('setMetadataTTL', () => {
-        it('calls luvio.storeSetDefaultTTLOverride with the right params', () => {
-            const TTL_VALUE = 1000;
-            const storeSetDefaultTTLOverrideSpy = jest.spyOn(luvio, 'storeSetDefaultTTLOverride');
+                setUiApiRecordTTL(TTL_VALUE);
+                expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledTimes(1);
+                expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledWith(
+                    API_NAMESPACE,
+                    RECORD_REPRESENTATION_NAME,
+                    TTL_VALUE
+                );
+            });
+        });
 
-            setMetadataTTL(TTL_VALUE);
-            expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledTimes(1);
-            expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledWith(TTL_VALUE);
+        describe('setMetadataTTL', () => {
+            it('calls luvio.storeSetDefaultTTLOverride with the right params', () => {
+                const TTL_VALUE = 1000;
+                const storeSetDefaultTTLOverrideSpy = jest.spyOn(
+                    luvio,
+                    'storeSetDefaultTTLOverride'
+                );
+
+                setMetadataTTL(TTL_VALUE);
+                expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledTimes(1);
+                expect(storeSetDefaultTTLOverrideSpy).toHaveBeenCalledWith(TTL_VALUE);
+            });
         });
     });
 });
--- packages/lds-worker-api/src/ttl.ts
+++ packages/lds-worker-api/src/ttl.ts
@@ -2,14 +2,31 @@ import { Luvio } from '@luvio/engine';
 import { API_NAMESPACE, RECORD_REPRESENTATION_NAME } from '@salesforce/lds-uiapi-record-utils';
 import { withDefaultLuvio } from '@salesforce/lds-runtime-mobile';
 
+let luvio: Luvio | undefined = undefined;
+
+let uiApiRecordTTL: number | undefined;
+let metadataTTL: number | undefined;
+
+withDefaultLuvio((_luvio: Luvio) => {
+    luvio = _luvio;
+    if (uiApiRecordTTL !== undefined) {
+        setUiApiRecordTTL(uiApiRecordTTL);
+    }
+    if (metadataTTL !== undefined) {
+        setMetadataTTL(metadataTTL);
+    }
+});
+
 export function setUiApiRecordTTL(ttl: number): void {
-    withDefaultLuvio((luvio: Luvio) => {
+    uiApiRecordTTL = ttl;
+    if (luvio !== undefined) {
         luvio.storeSetTTLOverride(API_NAMESPACE, RECORD_REPRESENTATION_NAME, ttl);
-    });
+    }
 }
 
 export function setMetadataTTL(ttl: number): void {
-    withDefaultLuvio((luvio: Luvio) => {
+    metadataTTL = ttl;
+    if (luvio !== undefined) {
         luvio.storeSetDefaultTTLOverride(ttl);
-    });
+    }
 }

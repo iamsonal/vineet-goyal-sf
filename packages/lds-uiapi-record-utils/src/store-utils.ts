96e5924fbbf6acfcb2d6bddf7d3587044b07c112
Justin Horst <justin.horst@salesforce.com>
2021-10-14 10:19:45 -0400

    update record utils to use string operators instead of regex (#1489)

--- packages/lds-uiapi-record-utils/src/store-utils.ts
+++ packages/lds-uiapi-record-utils/src/store-utils.ts
@@ -2,30 +2,27 @@ export const API_NAMESPACE = 'UiApi';
 export const RECORD_REPRESENTATION_NAME = 'RecordRepresentation';
 export const RECORD_ID_PREFIX = `${API_NAMESPACE}::${RECORD_REPRESENTATION_NAME}:`;
 
-const RECORD_ID_REGEXP = new RegExp(`^${RECORD_ID_PREFIX}([a-zA-Z0-9])+$`);
-const RECORD_FIELD_REGEXP = new RegExp(
-    `^${RECORD_ID_PREFIX}([a-zA-Z0-9]+)+__fields__([a-zA-Z0-9]+)+$`
-);
 const RECORD_FIELDS_KEY_JUNCTION = '__fields__';
-const RECORD_OR_RECORD_FIELD_REGEXP = new RegExp(`^${RECORD_ID_PREFIX}([a-zA-Z0-9]+)+.*$`);
 
 export function isStoreKeyRecordId(key: string) {
-    return RECORD_ID_REGEXP.test(key);
+    return (
+        key.indexOf(RECORD_REPRESENTATION_NAME) > -1 &&
+        key.indexOf(RECORD_FIELDS_KEY_JUNCTION) === -1
+    );
 }
 
 export function isStoreKeyRecordField(key: string) {
-    return RECORD_FIELD_REGEXP.test(key);
+    return (
+        key.indexOf(RECORD_REPRESENTATION_NAME) > -1 && key.indexOf(RECORD_FIELDS_KEY_JUNCTION) > -1
+    );
 }
 
 export function extractRecordIdFromStoreKey(key: string) {
-    if (key === undefined) {
+    if (key === undefined || key.indexOf(RECORD_REPRESENTATION_NAME) === -1) {
         return undefined;
     }
-    const matches = key.match(RECORD_OR_RECORD_FIELD_REGEXP);
-    if (!matches || matches.length !== 2) {
-        return undefined;
-    }
-    return matches[1];
+    const parts = key.split(':');
+    return parts[parts.length - 1].split('_')[0];
 }
 
 export function buildRecordFieldStoreKey(recordKey: string, fieldName: string) {

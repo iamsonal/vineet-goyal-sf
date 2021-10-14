34d18f6c0cd881fd73598afd0d31befe329cbad1
Paul Tiarks <ptiarks@salesforce.com>
2021-10-14 09:18:50 -0500

    fix(mobile): @W-9748376 - Luvio global Debug (#1473)
    * Fix a missed test assertion failure
    
    * Subscribe to withDefaultLuvio and set luvio to a global
    
    * Fix import of withDefaultLuvio

--- packages/lds-worker-api/src/main.ts
+++ packages/lds-worker-api/src/main.ts
@@ -9,6 +9,17 @@ import {
 import { nimbusDraftQueue } from './nimbusDraftQueue';
 import { draftQueue, draftManager } from './draftQueueImplementation';
 import { setUiApiRecordTTL, setMetadataTTL } from './ttl';
+import { withDefaultLuvio } from 'native/ldsEngineMobile';
+import { Luvio } from '@luvio/engine';
+
+if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line no-undef
+    withDefaultLuvio((luvio: Luvio) => {
+        // eslint-disable-next-line no-undef
+        const global = globalThis as any;
+        global.luvio = luvio;
+    });
+}
 
 export {
     subscribeToAdapter,

0ce05aea095ce068a1ecc553a64b4d4544eade0a
Stephen Carraway <57263085+scarrawaySF@users.noreply.github.com>
2021-10-19 15:32:58 -0400

    fix(tests): fix weird merge unit tests (#1514)

--- packages/lds-adapters-cms-delivery/src/wire/getCollectionItemsForChannel/__tests__/config.spec.ts
+++ packages/lds-adapters-cms-delivery/src/wire/getCollectionItemsForChannel/__tests__/config.spec.ts
@@ -7,15 +7,15 @@ describe('input config validation', () => {
         );
     });
 
-    it('calls storeLookup on valid input', async () => {
+    it('calls applyCachePolicy on valid input', async () => {
         const mockLds = {
-            storeLookup: jest.fn().mockReturnValue({ state: 'Fulfilled', data: {} }),
+            applyCachePolicy: jest.fn().mockReturnValue({ state: 'Fulfilled', data: {} }),
             snapshotAvailable: jest.fn().mockReturnValue(true),
         };
         await getCollectionItems(mockLds as any)({
             collectionKeyOrId: 'MC3XKWIYJC2JHUDDJB3LKZHG4ICM',
             channelId: '0apT300000000HLIAY',
         });
-        expect(mockLds.storeLookup.mock.calls.length).toBe(1);
+        expect(mockLds.applyCachePolicy.mock.calls.length).toBe(1);
     });
 });
--- packages/lds-adapters-cms-delivery/src/wire/getCollectionMetadataForSite/__tests__/config.spec.ts
+++ packages/lds-adapters-cms-delivery/src/wire/getCollectionMetadataForSite/__tests__/config.spec.ts
@@ -7,15 +7,15 @@ describe('input config validation', () => {
         );
     });
 
-    it('calls storeLookup on valid input', async () => {
+    it('calls applyCachePolicy on valid input', async () => {
         const mockLds = {
-            storeLookup: jest.fn().mockReturnValue({ state: 'Fulfilled', data: {} }),
+            applyCachePolicy: jest.fn().mockReturnValue({ state: 'Fulfilled', data: {} }),
             snapshotAvailable: jest.fn().mockReturnValue(true),
         };
         await getCollectionMetadata(mockLds as any)({
             collectionKeyOrId: 'MC3XKWIYJC2JHUDDJB3LKZHG4ICM',
             siteId: '0DMT300000000idOAA',
         });
-        expect(mockLds.storeLookup.mock.calls.length).toBe(1);
+        expect(mockLds.applyCachePolicy.mock.calls.length).toBe(1);
     });
 });

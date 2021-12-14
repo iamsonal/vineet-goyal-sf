05dd2771341c83a48bc969351d3b6f5c73990344
Ciaran E Hannigan <ciaran.hannigan@salesforce.com>
2021-12-14 15:02:09 -0500

    Revert "re-enable test now that cache policies handle pending snapshots (#1708)" (#1723)
    This reverts commit 84ae934563033db51db24113b5237bd10fd91bba.

--- packages/lds-adapters-graphql/src/wire/graphql/__karma__/basic/basic.spec.js
+++ packages/lds-adapters-graphql/src/wire/graphql/__karma__/basic/basic.spec.js
@@ -2283,7 +2283,8 @@ describe('graphql', () => {
                 });
             });
 
-            it('should refresh data when gql returns old records', async () => {
+            // TODO[@W-9987637]: test relies on cache policy handling pending snapshot correctly
+            xit('should refresh data when gql returns old records', async () => {
                 const query = parseQuery(/* GraphQL */ `
                     query {
                         uiapi {

10080dc857209387cf39ee2fd8121b502f423817
Ciaran E Hannigan <ciaran.hannigan@salesforce.com>
2021-12-15 12:13:01 -0500

    fix(@W-10288146): LDS network adapter is losing error body (#1720)
    * initial commit
    
    * comment
    
    * really this easy?
    
    * update tests for reject to have expected shape
    
    * remove test added in #1659
    
    * xit a test
    
    * removing it
    
    * remove unused interface
    
    * update testRejectFetchResponse

--- packages/lds-network-adapter/src/dispatch/main.ts
+++ packages/lds-network-adapter/src/dispatch/main.ts
@@ -1,15 +1,5 @@
 import { FetchResponse, NetworkAdapter, ResourceRequest } from '@luvio/engine';
 import { matchRecordsHandlers } from './records';
-interface ConnectInJavaError {
-    data: {
-        errorCode: string;
-        message: string;
-        statusCode: number;
-    };
-    id: string;
-    message: string;
-    stackTrace: string;
-}
 
 export interface SalesforceResourceRequest {
     networkAdapter: NetworkAdapter;
@@ -22,35 +12,7 @@ export const defaultDispatcher: Dispatcher = (
     req: SalesforceResourceRequest
 ): Promise<FetchResponse<unknown>> => {
     const { networkAdapter, resourceRequest } = req;
-    return networkAdapter(resourceRequest).then(null, (err) => {
-        // Handle ConnectedInJava exception shapes
-        if (err.data !== undefined && err.data.statusCode !== undefined) {
-            const { data } = err as ConnectInJavaError;
-            const errResp: FetchResponse<unknown> = {
-                status: data.statusCode,
-                body: data,
-                ok: err.ok,
-                statusText: err.statusText,
-                headers: err.headers,
-            };
-            throw errResp;
-        }
-
-        if (err.status !== undefined) {
-            const errResp: FetchResponse<unknown> = {
-                status: err.status,
-                body: {
-                    error: err.message,
-                },
-                ok: err.ok,
-                statusText: err.statusText,
-                headers: err.headers,
-            };
-            throw errResp;
-        }
-
-        throw err;
-    });
+    return networkAdapter(resourceRequest);
 };
 
 export function getDisaptcher(resourceRequest: ResourceRequest): Dispatcher {
